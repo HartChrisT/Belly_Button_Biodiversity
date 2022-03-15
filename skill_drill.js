49abcc1ae7bbce6cca1d11bd49f6c4376a9f891e
HartChrisT <92996865+HartChrisT@users.noreply.github.com>
2022-03-14 22:16:28 -0400

    adding more files from laptop

---
+++ skill_drill.js
@@ -0,0 +1,35 @@
+var numbers = [0,2,4,6,8];
+
+var numPlusFive = numbers.map(num => num + 5)
+
+console.log(numPlusFive)
+
+var cities = [
+    {
+      "Rank": 1,
+      "City": "San Antonio ",
+      "State": "Texas",
+      "Increase_from_2016": "24208",
+      "population": "1511946"
+    },
+    {
+      "Rank": 2,
+      "City": "Phoenix ",
+      "State": "Arizona",
+      "Increase_from_2016": "24036",
+      "population": "1626078"
+    },
+    {
+      "Rank": 3,
+      "City": "Dallas",
+      "State": "Texas",
+      "Increase_from_2016": "18935",
+      "population": "1341075"
+    }
+];
+
+var cityPop = cities.map(function(city) {
+    return city.population;
+});
+console.log(cityPop)
+
